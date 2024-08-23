c92d1f91a04780373eba0460608b7d0a4be34e37
Ankithv007 <ankithbv007@gmail.com>
2024-08-23 12:26:54 +0530

    Add files via upload

---
+++ delta-vpc/variable.tf
@@ -0,0 +1,9 @@
+variable "region"{ }
+variable "project_vpc"{ }
+variable "vpc_cidr"{ }
+variable "cidr_block_az1" {}
+variable "cidr_block_az2" {}
+variable "private-cidr_block_az1" {}
+variable "private-cidr_block_az2" {}
+variable "private-data-subnet-cidr_block_az1" {}
+variable "private-data-subnet-cidr_block_az2" {}
