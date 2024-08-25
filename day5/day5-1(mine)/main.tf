d3b62862ec4d24cea78046f9d641a7ea57b6200d
Ankith b v <ankithbv007@gmail.com>
2024-08-25 17:18:47 +0530

    Add files via upload

--- day5/day5-1(mine)/main.tf
+++ day5/day5-1(mine)/main.tf
@@ -1,93 +1,59 @@
+# Define the AWS provider configuration.
 provider "aws" {
-   region = "ap-south-1"
+  region = "ap-south-1"  # Replace with your desired AWS region.
 }
-resource "aws_vpc" "ank-vpc" {
-  cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "vpc for ankith"
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "terraform_key_pair"  # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-resource "aws_subnet" "ank-subnet-pub1" {
-  vpc_id = aws_vpc.ank-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name ="subnet for public sub 1a" 
-  }
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
 }
 
-resource "aws_subnet" "ank-subnet-pub2" {
-  vpc_id = aws_vpc.ank-vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
 
-  tags = {
-    Name ="subnet for public sub 1b" 
-  }
-}
-
-resource "aws_subnet" "ank-subnet-pri1" {
-  vpc_id = aws_vpc.ank-vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name ="subnet for private sub  1a" 
-  }
-}
-resource "aws_subnet" "ank-subnet-pri2" {
-  vpc_id = aws_vpc.ank-vpc.id
-  cidr_block = "10.0.4.0/24"
-  availability_zone = "ap-south-1b"
-
-  tags = {
-    Name ="subnet for private sub 1b" 
-  }
-}
-
-
-resource "aws_internet_gateway" "ank-igw" {
-  vpc_id = aws_vpc.ank-vpc.id
-
-  tags = {
-    Name =" attach to aws and igw"
-
-  }
-}
-
-resource "aws_route_table" "ank-rt" {
-  vpc_id = aws_vpc.ank-vpc.id
-
-  route  {
-    cidr_block ="0.0.0.0/0"
-    gateway_id =aws_internet_gateway.ank-igw.id
-  }
-
-  tags = {
-  Name =" rt for vpc"
-}
-
-}
-
-resource "aws_route_table_association" "pub-rt-1" {
-  subnet_id = aws_subnet.ank-subnet-pub1.id
-  route_table_id = aws_route_table.ank-rt.id
-}
-
-resource "aws_security_group" "ank-sg" {
-    name = "HTTP and SSH"
-    vpc_id = aws_vpc.ank-vpc.id
-
-     ingress {
+  ingress {
+    description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -97,22 +63,43 @@ resource "aws_security_group" "ank-sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
-}
-
-resource "aws_instance" "web_instance" {
-  ami           = "ami-0522ab6e1ddcc7055"
-  instance_type = "t2.micro"
-  key_name      = "key_pair"
-
-  subnet_id                   = aws_subnet.ank-subnet-pub1.id
-  vpc_security_group_ids      = [aws_security_group.ank-sg.id]
-  associate_public_ip_address = true
 
   tags = {
-    "Name" : "ankith EC2"
+    Name = "Web-sg"
+  }
+}
+
+resource "aws_instance" "server" {
+  ami                    = "ami-0522ab6e1ddcc7055"
+  instance_type          = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "nohup sudo python3 app.py > app.log 2>&1 &",
+    ]
   }
 }
